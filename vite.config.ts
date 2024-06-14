7b50627e8b55065b01f8ae9a94921642833037c8
haq <haqqnol@gmail.com>
2024-06-14 18:02:21 +0800

    1 add dicom image display (#2)
    * Working dicom parser library with image renderer
    
    * Update gitignore
    
    ---------
    
    Co-authored-by: haq_m <haqqnol@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), viteCommonjs()],
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [esbuildCommonjs(['dicomjs'])]
+		},
+		include: ['dicomjs']
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
