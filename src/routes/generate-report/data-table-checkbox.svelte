828126a4b2d6c7228ec5447cc26dd28fad846f4c
haq_m <alhaq.m@duerr-ndt.com>
2024-11-29 17:49:48 +0800

    Removed unused

--- src/routes/generate-report/data-table-checkbox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { Writable } from 'svelte/store';
-	import { Checkbox } from '@shadcn/ui/checkbox/index.js';
-
-	export let checked: Writable<boolean>;
-</script>
-
-<Checkbox bind:checked={$checked} />
