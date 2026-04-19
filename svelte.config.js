030929490fdd53bf20dc6e6595e2200f7e6add50
haq <haqqnol@gmail.com>
2026-04-19 18:31:47 +0800

    Fix: Config to work with vercel (#20)

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ out: 'build' }),
+		adapter: process.env.DEPLOY_TARGET === 'node' ? adapterNode({ out: 'build' }) : adapterVercel(),
 		alias: {
-			"@shadcn": "src/lib/shadcn/*",
-		  },
+			'@shadcn': 'src/lib/shadcn/*'
+		}
 	}
 };
 
